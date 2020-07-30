818af0fa51b3b58e0955669351121b5c275cf1b6
Moosa Khalid <54149373+moosakhalidLA@users.noreply.github.com>
2020-07-30 13:40:36 -0400

    Update outputs.tf
    Fixed typo in output label

--- aws_la_cloudplayground_multiple_workers_version/outputs.tf
+++ aws_la_cloudplayground_multiple_workers_version/outputs.tf
@@ -11,13 +11,13 @@ output "Jenkins-Main-Node-Public-IP" {
 output "Jenkins-Main-Node-Private-IP" {
   value = aws_instance.jenkins-master.private_ip
 }
-output "Jenkins-Worker-Public-IP(s)" {
+output "Jenkins-Worker-Public-IPs" {
   value = {
     for instance in aws_instance.jenkins-worker-oregon :
     instance.id => instance.public_ip
   }
 }
-output "Jenkins-Worker-Private-IP(s)" {
+output "Jenkins-Worker-Private-IPs" {
   value = {
     for instance in aws_instance.jenkins-worker-oregon :
     instance.id => instance.private_ip
