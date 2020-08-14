89e6acd65ee4ab203141a2dc6047169e73c9434b
Moosa Khalid <moosa.khalid@linuxacademy.com>
2020-08-13 23:28:06 -0400

    Updated, fixed DNS Hands-on lab

--- lab_deploying_dns_acm/alb_acm.tf
+++ lab_deploying_dns_acm/alb_acm.tf
@@ -9,10 +9,12 @@ resource "aws_acm_certificate" "aws-ssl-cert" {
 
 }
 
+#Validates ACM issued certificate via Route53
 resource "aws_acm_certificate_validation" "cert" {
   provider                = aws.region-master
   certificate_arn         = aws_acm_certificate.aws-ssl-cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  for_each                = aws_route53_record.cert_validation
+  validation_record_fqdns = [aws_route53_record.cert_validation[each.key].fqdn]
 }
 
 ####ACM CONFIG END
